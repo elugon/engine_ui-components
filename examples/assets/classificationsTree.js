beae86a84cb89dc67c53ac640d4695095e633ba0
Elu Gonzalez <101187859+elugon@users.noreply.github.com>
2024-11-19 16:32:44 +0100

    build updated

--- examples/assets/classificationsTree.js
+++ examples/assets/classificationsTree.js
@@ -1,10 +1,10 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import{T as l,C as p,a as d,e as f,m as b,x as y,y as w,i as g,b as u,s as T,z as C,j as v}from"./index-b5UjG9AG.js";import{b as E}from"./index-cxW_AbrR.js";import{t as z}from"./index-CagUo1tF.js";import"./lit-html-paDGiEfB.js";l.init();const e=new p,n=document.createElement("bim-viewport");n.name="viewer";const L=e.get(d),t=L.create(),o=new f(e);o.setup();t.scene=o;const c=new b(e,n);t.renderer=c;const r=new y(e);t.camera=r;n.addEventListener("resize",()=>{c.resize(),r.updateAspect()});const I=e.get(w);I.create(t);e.init();const h=e.get(g);await h.setup();const m=e.get(u);m.onFragmentsLoaded.add(s=>{t.scene&&t.scene.three.add(s)});const[x,B]=z.classificationTree({components:e,classifications:[]}),i=e.get(T);m.onFragmentsLoaded.add(async s=>{i.byEntity(s),await i.byPredefinedType(s),B({classifications:[{system:"entities",label:"Entities"},{system:"predefinedTypes",label:"Predefined Types"}]})});const F=C.create(()=>{const[s]=E.loadIfc({components:e});return v`
+import"./modulepreload-polyfill-B5Qt9EMX.js";import{T as l,C as p,a as d,e as f,m as b,x as y,y as w,i as g,b as u,t as T,j as C,k as v}from"./index-ByeqXO_H.js";import{b as E}from"./index-D_vz8Owi.js";import{t as L}from"./index-BCeOE3Ax.js";import"./lit-html-paDGiEfB.js";l.init();const e=new p,n=document.createElement("bim-viewport");n.name="viewer";const I=e.get(d),s=I.create(),o=new f(e);o.setup();s.scene=o;const c=new b(e,n);s.renderer=c;const r=new y(e);s.camera=r;n.addEventListener("resize",()=>{c.resize(),r.updateAspect()});const h=e.get(w);h.create(s);e.init();const x=e.get(g);await x.setup();const m=e.get(u);m.onFragmentsLoaded.add(t=>{s.scene&&s.scene.three.add(t)});const[z,B]=L.classificationTree({components:e,classifications:[]}),i=e.get(T);m.onFragmentsLoaded.add(async t=>{i.byEntity(t),await i.byPredefinedType(t),B({classifications:[{system:"entities",label:"Entities"},{system:"predefinedTypes",label:"Predefined Types"}]})});const F=C.create(()=>{const[t]=E.loadIfc({components:e});return v`
    <bim-panel label="Classifications Tree">
     <bim-panel-section label="Importing">
-      ${s}
+      ${t}
     </bim-panel-section>
     <bim-panel-section label="Classifications">
-      ${x}
+      ${z}
     </bim-panel-section>
    </bim-panel> 
   `}),a=document.createElement("bim-grid");a.layouts={main:{template:`
